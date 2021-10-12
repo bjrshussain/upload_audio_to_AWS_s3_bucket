8f6a8dc2c19a3472e00cf97c37a526ec4b4e3880
bjrshussain <bjrshussain@outlook.com>
2021-10-12 13:32:09 -0500

    all source code for s3.uplaod source code

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express');
+const multer = require('multer');
+const { memoryStorage } = require('multer')
+const storage = memoryStorage()
+const upload = multer({ storage })
+const uploadAudio = require('./aws')
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// uplaods post route
+app.post('/upload', upload.single('audiofile'), async (req, res) => {
+    const filename = 'hussainhussain';
+    const bucketname = 'octoberbuokett';
+    const file = req.file.buffer
+    // link is the returned object URL from S3
+    const link = await uploadAudio(filename, bucketname, file)
+    res.send(link)
+})
+
+app.listen(8000, () => {
+    console.log('serving on 8000')
+})
---
+++ aws.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    accessKeyId: process.env.access_key,
+    secretAccessKey: process.env.secret_access_key
+})
+
+const uploadAudio = (filename, bucketname, file) => {
+
+    return new Promise((resolve, reject) => {
+        const params = {
+            Key: filename,
+            Bucket: bucketname,
+            Body: file,
+            ContentType: 'audio/mpeg',
+            ACL: 'public-read'
+        }
+
+        s3.upload(params, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data.Location)
+            }
+        })
+    })
+}
+
+module.exports = uploadAudio
+
